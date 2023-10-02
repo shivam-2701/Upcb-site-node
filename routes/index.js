b9077357eadab3b06956440af992181c5af0e304
shivam-2701 <shivam.shah2701@gmail.com>
2023-10-02 15:34:35 +0530

    fix: :bug:

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const enRouter = require("./enRouter");
 const hiRouter = require("./hiRouter");
 
-router.use("/hi", hiRouter);
 router.use("/", enRouter);
-
-router.use((req, res, next) => {
-  return res.redirect("/hi");
-});
+router.use("/hi", hiRouter);
 
 module.exports = router;
